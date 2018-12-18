f78e1d6e22acee513d7bf924408bdf03aaa82f79
thedoormattt <45965277+thedoormattt@users.noreply.github.com>
2018-12-18 12:29:21 +0000

    Create Java_k7_getMiddleChar.java

---
+++ Java_k7_getMiddleChar.java
@@ -0,0 +1,17 @@
+class Kata {
+  public static String getMiddle(String word) {
+    int wordLength = word.length();
+    String output = "";
+    
+    if(wordLength%2 == 0) {
+      output = Character.toString(word.charAt((wordLength/2)-1));
+      output = 
+        output.concat(Character.toString(word.charAt(wordLength/2)));
+   }
+   if(wordLength%2 != 0) {
+      output = Character.toString(word.charAt((wordLength-1)/2));
+   }
+   
+   return output;
+  }
+}
