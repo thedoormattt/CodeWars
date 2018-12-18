cc70d44fce235236d1f7b74c7449387f82f95497
thedoormattt <45965277+thedoormattt@users.noreply.github.com>
2018-12-18 15:56:11 +0000

    Create SQL_k7_upAndDown.sql

---
+++ SQL/SQL_k7_upAndDown.sql
@@ -0,0 +1,12 @@
+-- Code for: https://www.codewars.com/kata/595a3ba3843b0cbf8e000004
+
+SELECT
+CASE
+  WHEN (SUM(number1))%2 = 0 THEN MAX(number1)
+  ELSE MIN(number1)
+END AS number1,
+CASE
+  WHEN (SUM(number2))%2 = 0 THEN MAX(number2)
+  ELSE MAX(number2)
+END AS number2
+FROM numbers;
