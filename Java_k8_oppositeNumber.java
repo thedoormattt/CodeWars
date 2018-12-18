dc2585c0ae98769a92eecff7defd2fe57c429bcd
thedoormattt <45965277+thedoormattt@users.noreply.github.com>
2018-12-18 12:35:01 +0000

    Create Java_k8_oppositeNumber.java

---
+++ Java_k8_oppositeNumber.java
@@ -0,0 +1,9 @@
+// Code for: https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            return number*-1;
+        }
+    }
